f8342d2b159e63ccc9b26f5dc75a5bfd9cb4885f
Yue Yin <yueyin.dev@gmail.com>
2024-02-14 21:32:19 -0500

    TwoMergeIterator

--- mini-lsm-starter/src/iterators/two_merge_iterator.rs
+++ mini-lsm-starter/src/iterators/two_merge_iterator.rs
@@ -10,7 +10,7 @@ use super::StorageIterator;
 pub struct TwoMergeIterator<A: StorageIterator, B: StorageIterator> {
     a: A,
     b: B,
-    // Add fields as need
+    pointing_to_first: bool,
 }
 
 impl<
@@ -18,8 +18,27 @@ impl<
         B: 'static + for<'a> StorageIterator<KeyType<'a> = A::KeyType<'a>>,
     > TwoMergeIterator<A, B>
 {
+    fn should_point_to_first(&self) -> bool {
+        if !self.a.is_valid() {
+            return false;
+        }
+
+        if !self.b.is_valid() {
+            return true;
+        }
+
+        self.a.key() <= self.b.key()
+    }
+
     pub fn create(a: A, b: B) -> Result<Self> {
-        unimplemented!()
+        let mut iter = Self {
+            a,
+            b,
+            pointing_to_first: false,
+        };
+        iter.pointing_to_first = iter.should_point_to_first();
+
+        Ok(iter)
     }
 }
 
@@ -31,18 +50,44 @@ impl<
     type KeyType<'a> = A::KeyType<'a>;
 
     fn key(&self) -> Self::KeyType<'_> {
-        unimplemented!()
+        if self.pointing_to_first {
+            self.a.key()
+        } else {
+            self.b.key()
+        }
     }
 
     fn value(&self) -> &[u8] {
-        unimplemented!()
+        if self.pointing_to_first {
+            self.a.value()
+        } else {
+            self.b.value()
+        }
     }
 
     fn is_valid(&self) -> bool {
-        unimplemented!()
+        if self.pointing_to_first {
+            self.a.is_valid()
+        } else {
+            self.b.is_valid()
+        }
     }
 
     fn next(&mut self) -> Result<()> {
-        unimplemented!()
+        if self.pointing_to_first {
+            if self.b.is_valid() && self.a.key() == self.b.key() {
+                self.b.next()?;
+            }
+            self.a.next()?;
+        } else {
+            if self.a.is_valid() && self.a.key() == self.b.key() {
+                self.a.next()?;
+            }
+            self.b.next()?;
+        }
+
+        self.pointing_to_first = self.should_point_to_first();
+
+        Ok(())
     }
 }
